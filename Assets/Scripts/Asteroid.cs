d3c40773d3e6b0edcf71ceb91370e92cfefe6dab
Aleesharyan <1027437@student.sae.edu.au>
2022-05-02 12:20:32 +1000

    added asteroids

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public Sprite[] sprites;
+
+    public float size = 1.0f;
+    public float minSize = 0.5f;
+    public float maxSize = 1.5f;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private Rigidbody2D _rigidbody;
+
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //this is going to pick a random sprite
+        _spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+
+        //this will give it a random rotation
+        this.transform.eulerAngles = new Vector3(0.0f, 0.0f, Random.value * 360.0f);
+
+        //and this will randomise its scale
+        //Vector3.one is a simplier way of writing 'new Vector3(this.size, this.size, this.size)
+        this.transform.localScale = Vector3.one * this.size;
+
+        //now to set the mass
+        _rigidbody.mass = this.size;
+
+    }
+
+    
+}
